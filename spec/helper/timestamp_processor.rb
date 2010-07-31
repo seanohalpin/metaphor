8ec218e337e6ea9aaabc62148381d53d358aa6b0
Craig R Webster <craig@barkingiguana.com>
2010-08-01 02:40:56 +0800

    Use RSpec sequences to remove unnecessary test objects.

--- spec/helper/timestamp_processor.rb
+++
@@ -1,6 +0,0 @@
-class TimestampProcessor
-  attr_reader :received_message_at
-  def process(*args)
-    @received_message_at = Time.now
-  end
-end
--- spec/metaphor_spec.rb
+++ spec/metaphor_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'helper/timestamp_processor'
 
 describe Metaphor do
   it "is version 0.1.0" do
@@ -16,17 +15,15 @@ describe Metaphor do
   describe "processing a message" do
     it "passes the message to each processor in turn" do
       m = Metaphor.new
-      processor_a = TimestampProcessor.new
-      processor_b = TimestampProcessor.new
-      m.processors << processor_a
-      m.processors << processor_b
-      m.process({}, "")
-
-      received_a = processor_a.received_message_at
-      received_a.should_not be_nil
-      received_b = processor_b.received_message_at
-      received_b.should_not be_nil
-      (received_a < received_b).should be_true
+      message = [ { 'message-id' => 'whatever' }, "message body" ]
+      processing = sequence('processing')
+      a = stub_everything('Processor A')
+      a.expects(:process).once.with(*message).in_sequence(processing)
+      b = stub_everything('Processor B')
+      b.expects(:process).once.with(*message).in_sequence(processing)
+      m.processors << a
+      m.processors << b
+      m.process(*message)
     end
 
     describe "if a processor returns a boolean false" do
