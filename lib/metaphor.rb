57cea19f136e27708b356981254b7855b463b786
Craig R Webster <craig@barkingiguana.com>
2010-08-01 01:18:43 +0800

    Allow specifying processors.

--- lib/metaphor.rb
+++ lib/metaphor.rb
@@ -1,3 +1,8 @@
-module Metaphor
+class Metaphor
   VERSION = '0.1.0'
+  attr_accessor :processors
+
+  def initialize
+    self.processors = []
+  end
 end
--- spec/metaphor_spec.rb
+++ spec/metaphor_spec.rb
@@ -4,4 +4,11 @@ describe Metaphor do
   it "is version 0.1.0" do
     Metaphor::VERSION.should eql('0.1.0')
   end
+
+  it "allows adding processors" do
+    m = Metaphor.new
+    lambda {
+      m.processors << Class.new do; def process(*args); end; end
+    }.should_not raise_error
+  end
 end
