4647ac2ab6f713a02fe121d346091a12836bac09
Craig R Webster <craig@barkingiguana.com>
2010-08-23 01:25:31 +0800

    Ruby 1.9.2 compatibility fix.

--- lib/metaphor/processor/json_processor.rb
+++ lib/metaphor/processor/json_processor.rb
@@ -8,8 +8,10 @@ class Metaphor
       def call(headers, body)
         result = JSON[body]
         headers['content-type'] = case result
-        when Hash:   'application/x-ruby'
-        when String: 'application/json'
+        when Hash
+          'application/x-ruby'
+        when String
+          'application/json'
         end
         [ headers, result ]
       end
